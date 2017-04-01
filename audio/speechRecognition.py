1e0a70178196770f10935ed04fd622bc8c8a8131
Luke Volpatti <luke.volpatti@gmail.com>
2017-04-01 15:36:57 -0400

    Speech recognition test

---
+++ audio/speechRecognition.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# Requires PyAudio and PySpeech.
+ 
+import speech_recognition as sr
+ 
+# Record Audio
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+ 
+# Speech recognition using Google Speech Recognition
+try:
+    # for testing purposes, we're just using the default API key
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # instead of `r.recognize_google(audio)`
+    print("You said: " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+text_file = open("TangledAnswers.txt","a")
+text_file.write(r.recognize_google(audio)+"\n")
+text_file.close()
