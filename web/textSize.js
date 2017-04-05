54c6696a66a92244a509a517709637f50c9c00b9
Luke Volpatti <luke.volpatti@gmail.com>
2017-04-05 19:36:40 -0400

    Added decrease font size

--- web/textSize.js
+++ web/textSize.js
@@ -10,6 +10,12 @@ function increaseSize(){
 }
 
 function decreaseSize(){
-	document.body.style.fontSize="6vh";
+	var min = 5;
+	var bodySize = document.body.style.fontSize.substring(0, document.body.style.fontSize.length -2);
+	if(bodySize === ""){
+		document.body.style.fontSize = "5vh";
+	} else if (parseInt(bodySize) > min){
+		document.body.style.fontSize = parseInt(bodySize) - 1 + "vh";
+	}
 	document.getElementById("mainTitle").style.fontSize="15vh";
 }
